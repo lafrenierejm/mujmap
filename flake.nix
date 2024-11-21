e30a8310221ada3b0a376c2fa7de5e9363646063
Joseph LaFreniere <git@lafreniere.xyz>
2024-11-21 16:29:03 -0500

    Replace cargo2nix with Crane and optimize Flake
    Crane obsoletes the need for `Cargo.nix` by parsing dependencies directly from
    `Cargo.lock` and improves build times by building and caching dependencies as
    their own Nix derivation.

--- flake.nix
+++ flake.nix
@@ -1,30 +1,105 @@
 {
   description = "Bridge for synchronizing email and tags between JMAP and notmuch";
   inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    crane.url = "github:ipetkov/crane";
     flake-utils.url = "github:numtide/flake-utils";
-    cargo2nix.url = "github:cargo2nix/cargo2nix/release-0.11.0";
-    nixpkgs.follows = "cargo2nix/nixpkgs";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    advisory-db = {
+      url = "github:rustsec/advisory-db";
+      flake = false;
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils, cargo2nix }:
-    flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [cargo2nix.overlays.default];
+  outputs = {
+    self,
+    nixpkgs,
+    crane,
+    flake-utils,
+    rust-overlay,
+    advisory-db,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      craneLib = crane.mkLib nixpkgs.legacyPackages.${system};
+
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [(import rust-overlay)];
+      };
+
+      filterMd = path: _type: null != builtins.match ".*md" path;
+      filterSrc = path: type: (filterMd path type) || (craneLib.filterCargoSources path type);
+
+      src = pkgs.lib.cleanSourceWith {
+        src = craneLib.path ./.; # The original, unfiltered source
+        filter = filterSrc;
+      };
+
+      buildInputs = with pkgs;
+        lib.optionals stdenv.isDarwin [
+          darwin.apple_sdk.frameworks.Security
+          pkg-config
+        ];
+
+      nativeBuildInputs = with pkgs;
+        lib.optionals stdenv.isDarwin [
+          # darwin-specific inputs
+          clang
+          libiconv
+        ];
+
+      # Build *just* the cargo dependencies, so we can reuse
+      # all of that work (e.g. via cachix) when running in CI
+      cargoArtifacts = craneLib.buildDepsOnly {inherit src buildInputs nativeBuildInputs;};
+
+      # Build ripsecrets itself, reusing the dependency artifacts from above.
+      mujmap = craneLib.buildPackage {
+        inherit cargoArtifacts src buildInputs nativeBuildInputs;
+        propagatedBuildInputs = [pkgs.notmuch];
+        doCheck = false;
+        meta = with pkgs.lib; {
+          description = "Bridge for synchronizing email and tags between JMAP and notmuch";
+          homepage = "https://github.com/elizagamedev/mujmap";
+          maintainers = [maintainers.lafrenierejm];
+          license = licenses.gpl3;
         };
-        rustPkgs = pkgs.rustBuilder.makePackageSet {
-          rustVersion = "1.61.0";
-          packageFun = import ./Cargo.nix;
+      };
+    in {
+      packages = {
+        inherit mujmap;
+        default = mujmap;
+      };
+
+      apps.default = flake-utils.lib.mkApp {drv = mujmap;};
+
+      # `nix flake check`
+      checks = {
+        audit = craneLib.cargoAudit {inherit src advisory-db;};
+
+        clippy = craneLib.cargoClippy {
+          inherit cargoArtifacts src nativeBuildInputs;
+          cargoClippyExtraArgs = "--all-targets -- --deny warnings";
         };
-      in
-      {
-        packages = rec {
-          mujmap = ((rustPkgs.workspace.mujmap {}).overrideAttrs(oa: {
-            propagatedBuildInputs = oa.propagatedBuildInputs ++ [ pkgs.notmuch ];
-          })).bin;
-          
-          default = mujmap;
+
+        doc = craneLib.cargoDoc {inherit cargoArtifacts src;};
+
+        fmt = craneLib.cargoFmt {inherit src;};
+
+        nextest = craneLib.cargoNextest {
+          inherit cargoArtifacts src nativeBuildInputs;
+          partitions = 1;
+          partitionType = "count";
         };
-      });
+      };
+
+      # `nix develop`
+      devShells.default = pkgs.mkShell {
+        inherit buildInputs nativeBuildInputs;
+        inputsFrom = builtins.attrValues self.checks;
+        packages = with pkgs; [cargo clippy rustc];
+      };
+    });
 }
