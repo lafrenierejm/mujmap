84d4640efe7a5e094b4d2ed825a3d8041d04f7c3
Joseph LaFreniere <git@lafreniere.xyz>
2025-09-26 07:30:47 -0500

    nix: Migrate to flake-parts

--- flake.nix
+++ flake.nix
@@ -2,113 +2,107 @@
   description = "Bridge for synchronizing email and tags between JMAP and notmuch";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs";
-    crane.url = "github:ipetkov/crane";
-    flake-utils.url = "github:numtide/flake-utils";
-    rust-overlay = {
-      url = "github:oxalica/rust-overlay";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
     advisory-db = {
       url = "github:rustsec/advisory-db";
       flake = false;
     };
-    pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    git-hooks-nix = {
+      url = "github:cachix/git-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nix-systems = {
+      url = "github:nix-systems/default";
+      flake = false;
+    };
+    rust-flake.url = "github:juspay/rust-flake";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    crane,
-    flake-utils,
-    rust-overlay,
-    advisory-db,
-    pre-commit-hooks,
-  }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      craneLib = crane.mkLib nixpkgs.legacyPackages.${system};
-
-      pkgs = import nixpkgs {
-        inherit system;
-        overlays = [(import rust-overlay)];
-      };
-
-      filterMd = path: _type: null != builtins.match ".*md" path;
-      filterSrc = path: type: (filterMd path type) || (craneLib.filterCargoSources path type);
-
-      src = pkgs.lib.cleanSourceWith {
-        src = craneLib.path ./.; # The original, unfiltered source
-        filter = filterSrc;
-      };
-
-      nativeBuildInputs = with pkgs;
-        lib.optionals stdenv.isDarwin [
-          pkg-config
-          clang
-          libiconv
+  outputs =
+    inputs@{ flake-parts, nix-systems, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } (
+      top@{
+        config,
+        withSystem,
+        moduleWithSystem,
+        ...
+      }:
+      {
+        imports = [
+          inputs.git-hooks-nix.flakeModule
+          inputs.rust-flake.flakeModules.default
+          inputs.rust-flake.flakeModules.nixpkgs
+          inputs.treefmt-nix.flakeModule
         ];
-
-      # Build *just* the cargo dependencies, so we can reuse
-      # all of that work (e.g. via cachix) when running in CI
-      cargoArtifacts = craneLib.buildDepsOnly {inherit src nativeBuildInputs;};
-
-      # Build ripsecrets itself, reusing the dependency artifacts from above.
-      mujmap = craneLib.buildPackage {
-        inherit cargoArtifacts src nativeBuildInputs;
-        propagatedBuildInputs = [pkgs.notmuch];
-        doCheck = false;
-        meta = with pkgs.lib; {
-          description = "Bridge for synchronizing email and tags between JMAP and notmuch";
-          homepage = "https://github.com/elizagamedev/mujmap";
-          maintainers = [maintainers.lafrenierejm];
-          license = licenses.gpl3;
-        };
-      };
-
-      pre-commit = pre-commit-hooks.lib."${system}".run;
-    in {
-      packages = {
-        inherit mujmap;
-        default = mujmap;
-      };
-
-      apps.default = flake-utils.lib.mkApp {drv = mujmap;};
-
-      # `nix flake check`
-      checks = {
-        audit = craneLib.cargoAudit {inherit src advisory-db;};
-
-        clippy = craneLib.cargoClippy {
-          inherit cargoArtifacts src nativeBuildInputs;
-          cargoClippyExtraArgs = "--all-targets -- --deny warnings";
-        };
-
-        doc = craneLib.cargoDoc {inherit cargoArtifacts src;};
-
-        fmt = craneLib.cargoFmt {inherit src;};
-
-        nextest = craneLib.cargoNextest {
-          inherit cargoArtifacts src nativeBuildInputs;
-          partitions = 1;
-          partitionType = "count";
-        };
-
-        pre-commit = pre-commit {
-          src = ./.;
-          hooks = {
-            alejandra.enable = true;
-            editorconfig-checker.enable = true;
-            rustfmt.enable = true;
-            typos.enable = true;
+        systems = (import nix-systems);
+        perSystem =
+          {
+            config,
+            self',
+            pkgs,
+            ...
+          }:
+          {
+            devShells.default = config.rust-project.crane-lib.devShell {
+              inputsFrom = [
+                config.packages.mujmap
+                config.pre-commit.devShell
+                config.treefmt.build.devShell
+              ];
+            };
+            packages = {
+              default = self'.packages.mujmap;
+            };
+            pre-commit = {
+              check.enable = true;
+              settings.hooks = {
+                editorconfig-checker.enable = true;
+                ripsecrets.enable = true;
+                taplo.enable = true;
+                treefmt.enable = true;
+                typos.enable = true;
+              };
+            };
+            rust-project =
+              let
+                filterMd = path: _type: null != builtins.match ".*md" path;
+                filterSrc =
+                  path: type: (filterMd path type) || (config.rust-project.crane-lib.filterCargoSources path type);
+              in
+              {
+                src = pkgs.lib.cleanSourceWith {
+                  src = ./.; # original, unfiltered source
+                  filter = filterSrc;
+                };
+                crates.mujmap = {
+                  crane.args = {
+                    nativeBuildInputs = with pkgs; [
+                      pkg-config
+                      clang
+                    ];
+                    propagatedBuildInputs = with pkgs; [
+                      notmuch
+                    ];
+                  };
+                  path = ./.;
+                };
+              };
+            treefmt.config = {
+              projectRootFile = ".git/config";
+              flakeCheck = false; # use pre-commit's check instead
+              programs = {
+                nixfmt.enable = true;
+                prettier.enable = true;
+                shellcheck.enable = true;
+                shfmt = {
+                  enable = true;
+                  indent_size = 0;
+                };
+                taplo.enable = true;
+              };
+            };
           };
-        };
-      };
-
-      # `nix develop`
-      devShells.default = pkgs.mkShell {
-        inherit nativeBuildInputs;
-        inherit (self.checks.${system}.pre-commit) shellHook;
-        inputsFrom = builtins.attrValues self.checks;
-        packages = with pkgs; [cargo clippy rustc];
-      };
-    });
+      }
+    );
 }
